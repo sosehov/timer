f26ed5f999f2cab7d16749e29f148e272f527cbc
Soseh <sosehv@gmail.com>
2024-12-18 20:15:05 -0500

    Wrapped the logic in a function

--- timer1.js
+++ timer1.js
@@ -1,27 +1,22 @@
-// Takes the command line arguments and disregards the first two elements
-const args = process.argv.slice(2);
-// Prints a message if no input
-if (args.length === 0) {
-  console.log('Please enter timer values.');
-  process.exit();
-}
-// Filters out non-numbers and negative numbers
-filteredArgs = args.filter(time => !isNaN(time) && time > 0);
-// Converts arguments to number
-const times = filteredArgs.map(arg => Number(arg));
-// Set the timer to beep
-times.forEach(time => {
-  const timeInMs = time * 1000;// Converts time to milliseconds
-  setTimeout( () =>{
-    process.stdout.write('\u0007');// Beep sound 
-    console.log(`Alarm! ${time} seconds passed.`);
-  }, timeInMs);
+const simpleTimer = function(arg) {
+  // Check if no argument is provided
+  if (arg.length === 0) {
+    console.log('Please provide input numbers');
+    process.exit();
+  } 
+  // Filter and map the input to positive numbers
+  const filteredArgs = arg.filter(time => !isNaN(time) && time > 0);
+  // Convert arguments to numbers
+  const times = filteredArgs.map(el => Number(el));
+  // Set the timer to beep for each time interval
+  times.forEach(time => {
+    const timeInMs = time * 1000;// Converts time to milliseconds
+    setTimeout( () =>{
+      process.stdout.write('\u0007');// Beep sound 
+      console.log(`Alarm! ${time} seconds passed.`);
+    }, timeInMs);
 });
+};
 
-
-
-
-
-
-
-  
+const args = process.argv.slice(2);
+simpleTimer(args);
